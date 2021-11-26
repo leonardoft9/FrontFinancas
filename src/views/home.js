113a4774fae72b69930f3e7a408a6dd32bc06f5a
leonardoft9 <leonardoft9@hotmail.com>
2021-11-26 08:14:54 -0300

    Remover botão extra cadastro de usuario

--- src/views/home.js
+++ src/views/home.js
@@ -35,11 +35,6 @@ class Home extends React.Component{
                 <hr className="my-4" />
                 <p>E essa é sua área administrativa, utilize um dos menus ou botões abaixo para navegar pelo sistema.</p>
                 <p className="lead">
-                    <a className="btn btn-primary btn-lg" 
-                    href="#/cadastro-usuarios" 
-                    role="button"><i className="pi pi-users"></i>  
-                     Cadastrar Usuário
-                    </a>
                     <a className="btn btn-danger btn-lg" 
                     href="#/cadastro-lancamentos" 
                     role="button"><i className="pi pi-money-bill"></i>  
